73c989cd3bf7e075a350f3876ca85dd1eb4a3370
Ruth Vang <r.vang012@gmail.com>
2020-10-11 16:42:01 -0500

    going liveish

--- public/js/burgers.js
+++ public/js/burgers.js
@@ -1,7 +1,7 @@
 $(function() {
-    $(".change-sleep").on("click", function(event) {
-      var id = $(this).data("id");
-      var newSleep = $(this).data("newsleep");
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
   
       var newBurgers = {
       name: $("#yourburger [name=burger_name]").val().trim(),
@@ -19,4 +19,30 @@ $(function() {
         }
       );
     });
-  })
+  });
+
+  $.ajax("/api/burgers", {
+    type: "POST",
+    data: newBurgers
+  }).then(
+    function() {
+      console.log("create new burger");
+      // Reload the page to get the updated list
+      location.reload();
+    }
+  );
+
+$(".delete-burger").on("click", function(event) {
+  var id = $(this).data("id");
+
+  // Send the DELETE request.
+  $.ajax("/api/burgers/" + id, {
+    type: "DELETE"
+  }).then(
+    function() {
+      console.log("deleted burgers", id);
+      // Reload the page to get the updated list
+      location.reload();
+    }
+  );
+});
