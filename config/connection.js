31342e50ddb5c1efda1895707531236d1db922c5
Ruth Vang <r.vang012@gmail.com>
2020-10-17 12:55:42 -0500

    updating all

--- config/connection.js
+++ config/connection.js
@@ -7,8 +7,8 @@ if (process.env.JAWSDB_URL) {
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-  user: '',
-  password: '',
+  user: credentials.root,
+  password: credentials.password,
   database: "burgers"
 });
 }
--- db/usernamepsswrd.sql
+++
@@ -1,4 +0,0 @@
-
-grant all privileges on burgers . * to 'burger_admin'@'localhost';
-alter user 'burger_admin'@'localhost' identified with mysql_native_password by 'password';
-flush privileges;
